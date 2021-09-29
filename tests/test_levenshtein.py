eefda59d5cf600be817726d79bcf829784463c15
domonik <schdruzzi@gmail.com>
2021-09-29 16:22:37 +0200

    added levenshtein distance exercise

---
+++ tests/test_levenshtein.py
@@ -0,0 +1,18 @@
+import pytest
+from levenshtein import levenshtein
+
+
+@pytest.mark.parametrize(
+    "seq1,seq2,expected",
+    [
+        ("AGATATA", "TATATATA", 2),
+        ("AGTCCT", "CGCTCA", 4),
+        ("TGCATAT", "ATCCGAT", 4),
+        ("ATCCGAT", "TGCATAT", 4),
+        ("", "", 0),
+        ("A", "", 1),
+     ]
+)
+def test_levenstein(seq1, seq2, expected):
+    test = levenshtein(seq1, seq2)
+    assert test == expected
