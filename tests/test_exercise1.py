f4bdfda9168057c3e21c0d14e46d13d3174a4ee1
Alexander-Mitrofanov <mitrofan@informatik.uni-freiburg.de>
2021-10-11 20:54:06 +0200

    fixes

--- tests/test_exercise1.py
+++ tests/test_exercise1.py
@@ -4,18 +4,18 @@ from exercise_sheet2 import exercise_1
 
 def check_none(*args):
     if None in args:
-        print("you have not fill in all the fields")
+        print("You have not filled in all the fields!")
         raise ValueError
 
 
 def test_exercise_1():
     a, b, c, d, e, f = exercise_1()
     check_none(a, b, c, d, e, f)
-    assert a is 0
-    assert b is 1
-    assert c is 2
-    assert d is 4
-    assert e is 4
-    assert f is 4
+    assert a == 0
+    assert b == 1
+    assert c == 2
+    assert d == 4
+    assert e == 4
+    assert f == 4
 
 
--- tests/test_exercise2.py
+++ tests/test_exercise2.py
@@ -3,13 +3,13 @@ from exercise_sheet2 import exercise_2
 
 def check_none(*args):
     if None in args:
-        print("you have not fill in all the fields")
+        print("You have not filled in all the fields!")
         raise ValueError
 
 
 def test_exercise_2():
     a, b, c, d = exercise_2()
-    check_none(a, b, c, d, e, f)
+    check_none(a, b, c, d)
     assert a is False
     assert b is True
     assert c is False
