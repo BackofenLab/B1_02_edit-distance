1d65b5b79c5b519a8ed4302badc216d35176c0d6
Cristóbal Gallardo <gallardo@informatik.uni-freiburg.de>
2021-10-25 04:04:36 +0200

    Include comments

--- src/exercise_sheet2.py
+++ src/exercise_sheet2.py
@@ -1,6 +1,9 @@
 """
 Exercise 1 - Levenshtein Distance
-Compute the minimal Levenshtein edit distance for the following pairs of sequences.
+
+- Compute the minimal Levenshtein edit distance for the following pairs of sequences.
+- The response should be an integer value.
+
 """
 def exercise_1a(): return None
 def exercise_1b(): return None
@@ -10,7 +13,10 @@ def exercise_1e(): return None
 
 """
 Exercise 2 - Metric function
-Check if the corresponding functions are metric.
+
+- Check if the corresponding functions are metric.
+- The respond should be a boolean value (True or False)
+
 """
 def exercise_2a(): return None
 def exercise_2b(): return None
@@ -19,6 +25,9 @@ def exercise_2d(): return None
 
 """
 Exercise 3 - Programming assignment: Levenshtein Distance
+
+- Write the corresponing function
+
 """
 def levenshtein_substitution(sequence1, sequence2):
     number_substitutions = None
