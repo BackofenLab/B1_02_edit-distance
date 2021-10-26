4119554a0bf7e62fc892b5c160f91566af2229ad
Alexander-Mitrofanov <mitrofan@informatik.uni-freiburg.de>
2021-10-26 17:03:19 +0200

    description task 3

--- exercise_sheet2.py
+++ exercise_sheet2.py
@@ -115,8 +115,9 @@ def levenshtein_deletions(sequence1, sequence2):
      Implement the function levenshtein_deletion() which takes two sequences
      of different length and returns the positions of characters from the
      longest sequences which should be deleted to transform the sequence into
-     the other one. If such deletion can not be done the function should
-     return None.
+     the other one. The function should return the list of indexes.
+     If no operation is needed the function should return an empty list.
+     If such deletion can not be done the function should return None.
     """
-    deletions_indexes = None
+    deletions_indexes = []
     return deletions_indexes
