4bd8c41464e93093340e6af85d4157f9a2d09b59
Alexander Mitrofanov <46710688+Alexander-Mitrofanov@users.noreply.github.com>
2022-10-18 16:25:32 +0200

    Update exercise_sheet2.py

--- exercise_sheet2.py
+++ exercise_sheet2.py
@@ -8,94 +8,10 @@ first_name = ""
 last_name = ""
 student_id = ""
 
-
-
 """
-Exercise 1 - Levenshtein Distance
+Programming assignment: Levenshtein Distance
 
-- Compute the minimal Levenshtein edit distance for the following pairs of
-  sequences.
-- The response should be an integer value.
-
-"""
-
-
-def exercise_1a():
-    """
-    S1= "A", S2= "T"
-    """
-    return None
-
-
-def exercise_1b():
-    """
-    S1= "AGATATA", S2= "TATATATA"
-    """
-    return None
-
-
-def exercise_1c():
-    """
-    S1= "AGTCCT", S2= "CGCTCA"
-    """
-    return None
-
-
-def exercise_1d():
-    """
-    S1= "TGCATAT", S2= "ATCCGAT"
-    """
-    return None
-
-
-def exercise_1e():
-    """
-    S1= "ATCCGAT", S2= "TGCATAT"
-    """
-    return None
-
-
-"""
-Exercise 2 - Metric function
-
-- Check if the corresponding functions are metric.
-- The respond should be a boolean value (True or False)
-
-"""
-
-
-def exercise_2a():
-    """
-    w(x, y) = x - y
-    """
-    return None
-
-
-def exercise_2b():
-    """
-    w(x, y) = |x - y|
-    """
-    return None
-
-
-def exercise_2c():
-    """
-    w(x, y) = x + y
-    """
-    return None
-
-
-def exercise_2d():
-    """
-    w(x, y) = 1 if x != y else 0
-    """
-    return None
-
-
-"""
-Exercise 3 - Programming assignment: Levenshtein Distance
-
-- Implement the corresponding function
+- Implement the corresponding functions
 
 """
 
