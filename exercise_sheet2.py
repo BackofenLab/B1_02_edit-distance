e27b294b6b4aafba2f2f555cf8c1374ea1cecd50
Alexander Mitrofanov <46710688+Alexander-Mitrofanov@users.noreply.github.com>
2022-11-08 14:01:33 +0100

    Update exercise_sheet2.py

--- exercise_sheet2.py
+++ exercise_sheet2.py
@@ -28,12 +28,12 @@ def levenshtein_substitution(sequence1, sequence2):
 
 def levenshtein_deletions(sequence1, sequence2):
     """
-     Implement the function levenshtein_deletion() which takes two sequences
-     of different length and returns the positions of characters from the
-     longest sequences which should be deleted to transform the sequence into
-     the other one. The function should return the list of indexes.
-     If no operation is needed the function should return an empty list.
-     If such deletion can not be done the function should return None.
+    Implement the function levenshtein_deletion() which takes two sequences
+    and returns the positions of characters from the longest sequences which
+    should be deleted to transform the sequence into the other one.
+    This should be returned as a list of indices (int). 
+    If such deletion can not be done the function should return None. 
+    Also, if there are no editing operations needed the function should return an empty list.
     """
     deletions_indexes = []
     return deletions_indexes
